4ff73e4e49cc527a1400fade5e31d6efa21ee010
Marcelo Alves <161260493+marcelodev30@users.noreply.github.com>
2025-09-20 20:33:52 -0300

    add docker

--- smartIF/server/src/server.ts
+++ smartIF/server/src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
 import * as env from 'dotenv'
+import { request } from 'http';
 
 env.config();
 const app = express();
@@ -7,6 +8,9 @@ const port = process.env.PORT
 
 app.use(express.json());
 
+app.get('/', (request, response) => {
+    return response.json({ massege: 'ola porra' })
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
